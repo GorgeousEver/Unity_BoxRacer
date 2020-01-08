6af860b0dbe7ee4d7774806191e7c0d68bc62415
可 涂 <775769739@qq.com>
2020-01-08 23:14:58 +0800

    Add movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce;// Can not push the object if this value is too small
+    public float sideawayForce;
+
+    private bool isLeft = false;
+    private bool isRight = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isLeft = Input.GetKey("a");
+        isRight = Input.GetKey("d");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (isLeft)
+        {
+            rb.AddForce(-sideawayForce * Time.deltaTime, 0, 0);
+        }
+
+        if (isRight)
+        {
+            rb.AddForce(sideawayForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Unity_BoxRacer
 A hello-world game made with Unity when following the tutorial https://www.youtube.com/watch?v=IlKaB1etrik&amp;list=PLPV2KyIb3jR5QFsefuO2RlAgWEz6EvVi6&amp;index=2
+
+
+Note:
+1.  Using Time.DeltaTime to create smooth animation
+
+2.  Put all stuffs update about physics into FixedUpdate(), this function has a fixed timestep
+
+3.  Using RigidBody.AddForce() can craete more real movement
